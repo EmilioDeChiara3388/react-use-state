a03ec4ae2de0a26783082f88ea07b856009adc05
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-11-15 18:05:14 +0100

    add: onClick

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import AppHeader from './components/AppHeader'
 import languages from "./languages.js"
 import './App.css'
@@ -5,7 +6,12 @@ import './App.css'
 
 function App() {
 
+  const [active, setActive] = useState(0)
 
+  function handleClick(e) {
+    const newActive = Number(e.target.getAttribute("data-index"))
+    setActive(newActive)
+  }
   return (
     <>
       <AppHeader />
@@ -13,21 +19,19 @@ function App() {
         <div className="container">
           <div className='buttonRow'>
             {languages.map((item, index) =>
-
               <div key={item.id}>
-                <button>{item.title}</button>
+                <button onClick={handleClick} data-index={index}>{item.title}</button>
+                <div className={active == index ? "descriptionBox" : "hide"} key={index}>
+                  <h3>{item.title}</h3>
+                  <p>{item.description}</p>
+                </div>
               </div>
-
             )}
           </div>
 
-          {languages.map((item, index) =>
-            <div className='descriptionBox' key={index}>
-              <h3>{item.title}</h3>
-              <p>{item.description}</p>
-            </div>
-          )}
-
+          {/* {languages.map((item, index) =>
+            
+          )} */}
         </div>
       </main>
     </>
